7003a22d14b3f0abe6fbfb60b221b27f8e7d87b6
Ostabo <osi.borkenhagen@gmail.com>
2022-10-30 17:44:47 +0100

    first fft

--- p5/sound.js
+++ p5/sound.js
@@ -1,22 +1,48 @@
-let mySound;
+let sound;
+let fft;
 function preload() {
   soundFormats('mp3', 'ogg');
-  mySound = loadSound('../Vorlesung/Mario Batkovic_Accordion_ausschnitt');
+  sound = loadSound('../Vorlesung/Mario Batkovic_Accordion_ausschnitt');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  const peaks = mySound.getPeaks();
-  console.log(peaks);
-  let i = 0;
-  peaks.forEach(element => {
-    rect(++i * 60, element * 100000 + 200, 50, 50);
-  });
-  text(peaks.size, 20, 20);
-
-  mySound.play();
+  const canvas = createCanvas(windowWidth, windowHeight);
+  canvas.mouseClicked(togglePlay);
+  
+  fft = new p5.FFT();
+  sound.amp(0.2);
 }
 
 function draw() {
+  background(150);
+
+  let spectrum = fft.analyze();
+  noStroke();
+  fill(0);
+  for (let i = 0; i< spectrum.length; i++){
+    let x = map(i, 0, spectrum.length, 0, width * 2);
+    let y = map(i, 0, spectrum.length, 0, height * 2);
+    let h = -height + map(spectrum[i], 0, 255, height, 0);
+    circle(x, y, h);
+  }
+
+  let waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(255);
+  strokeWeight(2);
+  for (let i = 0; i< waveform.length; i++){
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map( waveform[i], -1, 1, 0, height);
+    point(x,y);
+  }
+  endShape();
+}
+
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+  }
 }
